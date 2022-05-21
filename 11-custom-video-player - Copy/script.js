ba8f5ede03fc89c962ddc4a93194a1abe8a5d24f
annnacaesar <Anechka22334@gmail.com>
2022-05-21 10:46:27 +0300

    Update script.js

--- 11-custom-video-player - Copy/script.js
+++ 11-custom-video-player - Copy/script.js
@@ -4,13 +4,21 @@ const video = player.querySelector('.viewer');
 const skipBtn = player.querySelectorAll('[data-skip]');
 const range = player.querySelectorAll('.player__slider');
 const progressBar = player.querySelector('.progress__filled');
+const progress = player.querySelector('.progress');
 
+let mouseDown = false;
 
 toggle.addEventListener('click', togglePlay)
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateBtn)
 video.addEventListener('pause', updateBtn)
 video.addEventListener('timeupdate', hundleProgress)
+progress.addEventListener('click', scrub)
+progress.addEventListener('mousedown', ()=> (mouseDown = true));
+progress.addEventListener('mouseup', ()=> (mouseDown = false));
+progress.addEventListener('mousemove', (event)=> mouseDown && scrub(event));
+
+
 
 function togglePlay () {
 	const method = video.paused ? 'play' : 'pause';
@@ -43,4 +51,11 @@ function hundleProgress(params) {
 	const parcent = video.currentTime / video.duration * 100;
 	progressBar.style.flexBasis = `${parcent}%`
 }
+
+function scrub (event) {
+	const scrubTime = (event.offsetX/progress.offsetWidth)*video.duration;
+	video.currentTime = scrubTime;
+}
+
+
 
